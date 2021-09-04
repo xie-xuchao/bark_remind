e15e2bcf27ce3411a480b40957c29f9d998c74c1
xie-xuchao <77599712+xie-xuchao@users.noreply.github.com>
2021-09-04 13:41:01 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,36 @@
+
+/**
+ * @fileoverview Example to compose HTTP request
+ * and handle the response.
+ *
+ */
+var url = "";
+const urlList = [
+{urll:"http://ddns.cloudslave.cn:8081/7xYty6EbxLfse3tHwBWLjK/上班打卡提醒/速度打卡"},
+{urll:"http://ddns.cloudslave.cn:8081/7xYty6EbxLfse3tHwBWLjK/上班打卡提醒/速度打卡"}
+];
+
+encodeURI("http://ddns.cloudslave.cn:8081/7xYty6EbxLfse3tHwBWLjK/上班打卡提醒/速度打卡");
+const method = "GET";
+const headers = {"Field": "test-header-param"};
+const data = {"info": "abc"};
+
+for(var i=0,l=urlList.length;i<l;i++){
+url = encodeURI(urlList[i].urll);
+const myRequest = {
+    url: url,
+    method: method, // Optional, default GET.
+    headers: headers, // Optional.
+    body: JSON.stringify(data) // Optional.
+};
+
+$task.fetch(myRequest).then(response => {
+    // response.statusCode, response.headers, response.body
+    console.log(response.body);
+    $notify("上班打卡提醒", "提醒成功", response.body); // Success!
+    $done();
+}, reason => {
+    // reason.error
+    $notify("上班打卡提醒", "提醒失败", reason.error); // Error!
+    $done();
+})};
